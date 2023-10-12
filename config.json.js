9f56129c8b2ff4c2f44cad62ac142bf01a2a5b5e
vj23456 <53201527+vj23456@users.noreply.github.com>
2023-10-12 23:24:37 +0800

    Add files via upload

---
+++ config.json.js
@@ -0,0 +1,11 @@
+{
+    "author": "vj23456", 
+    "description": "光猫助手: 快速设置，通过路由直接访问猫后台", 
+    "home_url": "Module_lookcat.asp", 
+    "link": "https://github.com/vj23456/LookCat", 
+    "md5": "44ca9b43b762bb0b24b49d6c40fb044f", 
+    "module": "lookcat", 
+    "tags": "工具", 
+    "title": "光猫助手", 
+    "version": "1.0"
+}
